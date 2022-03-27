108c8673556b8cb163e94cfe4c1e40a8a88b6abf
Lydia Ying <lcy2107@columbia.edu>
2022-03-26 23:42:16 -0400

    Final working version of hi.py

--- hi.py
+++ hi.py
@@ -1,22 +1,23 @@
 import json
 import twitter
 
+from collections import Counter
+
 def process_likes(json_data):
-    #dict = {}
-    #for data in json_data:
-    #    dict.update({data["author_id"]: 1})
-    
-    #ids = dict.keys()
-    #print(ids)
-
-    #for x in ids:
-    #    length = len(dict.fromkeys(x, 1))
-
-    ls = []
+    dictionary = {}
     for data in json_data:
-        ls.append(data["author_id"])
-    dictionary = Counter(ls)
-    print(dictionary)
+        if dictionary.get(data["author_id"]) == None:
+            dictionary.update({data["author_id"]: 1})
+        else:
+            x = dictionary.pop(data["author_id"])
+            dictionary.update({data["author_id"]: x+1})
+
+    sorted_dict = dict(sorted(dictionary.items(),
+                           key=lambda item: item[1],
+                           reverse=True))
+
+    return sorted_dict
+    
 
 def id_to_handle(idnum):
     api_endpoint = "2/users/"+str(idnum)
@@ -26,7 +27,6 @@ def id_to_handle(idnum):
         }
     }
     json_response = twitter.query(api_endpoint, query_params)
-    print(json_response)
     return json_response
 
 def handle_to_id(handle):
@@ -37,19 +37,37 @@ def handle_to_id(handle):
         }
     }
     json_response = twitter.query(api_endpoint, query_params)
-    print(json_response)
     return json_response
 
-if __name__ == "__main__":
-    api_endpoint = "2/users/166747718/liked_tweets"
-    query_params = {
-        'max_results':  5,
+def finalfunction(username):
+    id = (handle_to_id(username)["data"]).get("id")
+    api_endpoint = "2/users/"+str(id)+"/liked_tweets"
+    query_params =  {
+        'max_results':  100,
         'tweet.fields': {"author_id"},
         'next_token': {}
     }
     json_response = twitter.query(api_endpoint, query_params)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    dictionary = process_likes(json_response["data"])
 
-    # test
-    id_to_handle(166747718)
-    handle_to_id("tylerthecreator")
+    array = []
+    counter = 0
+    for x in dictionary:
+        if(counter==5):
+            break
+        response = id_to_handle(x)
+        temp = {}
+        temp.update({"Name": response["data"].get('name')})
+        temp.update({"Handle": response["data"].get('username')})
+        temp.update({"Photo": response["data"].get('profile_image_url')})
+        temp.update({"Bio": response["data"].get('description')})
+        array.append(temp)
+        counter+=1
+
+    return array
+    
+
+
+if __name__ == "__main__":
+    test = finalfunction("ronithhh")
+    print(test)
