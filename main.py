b86eb2d76f1eec456d324809b32c48f4fc0cb8e2
Sean Yu <sean.xiaochen.yu@gmail.com>
2022-03-27 12:31:38 -0400

    took out types and added cors support

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
 import search
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello():
@@ -9,7 +13,12 @@ def hello():
 
 @app.route('/<handle>')
 def main(handle):
-    return search.favorite_users(
+    search_dict = search.favorite_users(
         handle=handle, 
         max_processed_tweets=10, 
         max_results=5)
+    
+    response = jsonify(search_dict)
+    response.status_code = 200
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
